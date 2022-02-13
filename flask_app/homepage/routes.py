b7ef2b725e8ff1a8ea4c202fdf66bb916898728d
Tony Chen <git@tonychen.page>
2022-02-13 10:40:31 -0500

    Update 2022.02.13-10:40:31

--- flask_app/homepage/routes.py
+++ flask_app/homepage/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for
 
 homepage = Blueprint("homepage", __name__)
 
@@ -6,3 +6,8 @@ homepage = Blueprint("homepage", __name__)
 @homepage.route("/")
 def index():
     return render_template("index.html")
+
+
+@homepage.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='images/favicon.ico'))
